31cc60d6a87dd756b9f282bcf9fb542ba535d3cd
Denis Sorish <127504628+Revoool@users.noreply.github.com>
2025-12-24 10:52:42 +0200

    Initial Commit

---
+++ chatapp/chatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatappApp.swift
+//  chatapp
+//
+//  Created by Denis Sokolov on 24.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct chatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chatappTests/chatappTests.swift
@@ -0,0 +1,17 @@
+//
+//  chatappTests.swift
+//  chatappTests
+//
+//  Created by Denis Sokolov on 24.12.2025.
+//
+
+import Testing
+@testable import chatapp
+
+struct chatappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
